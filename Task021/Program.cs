dacde36b665669a9375c915e1bff6d722f718863
Tra-Ta <madtazik@gmail.com>
2022-11-24 22:47:19 +0300

    Complete Task023 and redacted Task021

--- Task021/Program.cs
+++ Task021/Program.cs
@@ -14,7 +14,7 @@ int z2 = Convert.ToInt32(Console.ReadLine());
 
 double Distance(int x1, int y1, int z1, int x2, int y2, int z2)
 {
-    return Math.Sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1)+(z2-z1)*(z2-z1));
+    return Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1) + (z2 - z1) * (z2 - z1));
 }
 double distance = Math.Round(Distance(x1, y1, z1, x2, y2, z2), 2, MidpointRounding.ToZero);
 Console.WriteLine(distance);
--- Task023/Program.cs
+++ Task023/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Enter natural number ->  ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+void CubeNumbers(int num)
+{
+    int num1 = 1;
+    while (num1 <= num)
+    {
+        double cube = num1 * num1 * num1;
+        Console.WriteLine($"{num1,2} {cube,4}");
+        num1++;
+    }
+}
+if (number > 0)
+    CubeNumbers(number);
+else Console.WriteLine("The number is not natural");
