1bbe54653740642043992b5a8260ba30286f8a44
Tra-Ta <madtazik@gmail.com>
2022-11-25 00:02:44 +0300

    Complete Task019

--- Task019/Program.cs
+++ Task019/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Enter a five-digit number -> ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+void Palindrome(int num)
+{
+    int num1 = num / 10000;
+    int num2 = (num / 1000) % 10;
+    int num3 = (num / 10) % 10;
+    int num4 = num % 10;
+    if (num1 == num4 || num2 == num3)
+        Console.WriteLine("The number is a palindrome");
+    else Console.WriteLine("The number is not a palindrome");
+}
+if (number > 100000 || number < 9999) Console.WriteLine("The number is not five-digit");
+else Palindrome(number);
