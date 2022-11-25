b4d77fe01458025eba4796f0802828adcb936272
Tra-Ta <madtazik@gmail.com>
2022-11-25 22:53:33 +0300

    Redacted Task019

--- Task019/Program.cs
+++ Task019/Program.cs
@@ -1,15 +1,13 @@
 ﻿Console.WriteLine("Enter a five-digit number -> ");
 int number = Convert.ToInt32(Console.ReadLine());
 
-void Palindrome(int num)
+bool Palindrome(int num)
 {
     int num1 = num / 10000;
     int num2 = (num / 1000) % 10;
     int num3 = (num / 10) % 10;
     int num4 = num % 10;
-    if (num1 == num4 || num2 == num3)
-        Console.WriteLine("The number is a palindrome");
-    else Console.WriteLine("The number is not a palindrome");
+    return num1 == num4 && num2 == num3;
 }
 if (number > 100000 || number < 9999) Console.WriteLine("The number is not five-digit");
-else Palindrome(number);
+else Console.WriteLine( Palindrome(number)? "The number is a palindrome" : "The number is not a palindrome");
